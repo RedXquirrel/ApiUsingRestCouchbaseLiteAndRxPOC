9a6098f250c35a42ad9811d763127a1d6666173f
Xamtastic <anthony.harrison@xamtastic.com>
2016-02-28 18:44:17 +0000

    [WIP] Removed BDD project.

--- ApiPOCTddTests/APITests.cs
+++ ApiPOCTddTests/APITests.cs
@@ -10,6 +10,18 @@ namespace ApiPOCTddTests
     [TestFixture]
     public class APITests
     {
+        [SetUp]
+        public void Start()
+        {
+            
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            
+        }
+
         [Test]
         public void TempTests()
         {
