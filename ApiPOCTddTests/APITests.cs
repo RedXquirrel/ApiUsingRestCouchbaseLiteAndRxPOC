258d07df1835d19a6e46603697e2ae8b6db86b80
Xamtastic <anthony.harrison@xamtastic.com>
2016-02-28 17:33:57 +0000

    [Feature] Create Shell - Shared Project, TDD Project, BDD Project, Upgrade Xamarin.Forms to latest.

---
+++ ApiPOCTddTests/APITests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPOCTddTests
+{
+    [TestFixture]
+    public class APITests
+    {
+        [Test]
+        public void TempTests()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
